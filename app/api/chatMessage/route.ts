2f9e5c5170a8640b4bf69fd17e83467f5b573596
kenmaqqe <dimetriytalibal@gmail.com>
2026-03-17 21:06:50 +0200

    use another llm model

--- app/api/chatMessage/route.ts
+++ app/api/chatMessage/route.ts
@@ -10,7 +10,7 @@ export async function POST(req: Request) {
   const { message } = await req.json();
 
   const stream = await client.chat.completions.create({
-    model: "meta-llama/llama-4-maverick-17b-128e-instruct",
+    model: "meta-llama/llama-4-scout-17b-16e-instruct",
     messages: [
       {
         role: "system",
